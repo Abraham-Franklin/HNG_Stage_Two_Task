87d38982757d55d60337e27701c46f2cce7bce30
Abraham-Franklin <110688915+Abraham-Franklin@users.noreply.github.com>
2024-07-08 23:52:01 +0100

    Update settings.py

--- stage_two_task/stage_two_task/settings.py
+++ stage_two_task/stage_two_task/settings.py
@@ -103,16 +103,16 @@ WSGI_APPLICATION = 'stage_two_task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'stage_two_task',
-#         'USER': 'franklinao',
-#         'PASSWORD': 'Oafo120504',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stage_two_task',
+        'USER': 'franklinao',
+        'PASSWORD': 'Oafo120504',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
